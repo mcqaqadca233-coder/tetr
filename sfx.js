8d7853fb8c5706b46fee9ef489308aebe87dbb63
mcqaqadca233-coder <mcqaqadca233@gmail.com>
2026-06-12 23:31:38 +0800

    Update sfx.js

--- sfx.js
+++ sfx.js
@@ -4,3 +4,26 @@ function sfx(src="sfx/kus.ogg"){
     audio.currentTime = 0;
     audio.play().catch(e=>console.log(e))
 }
+
+// 需要预加载的所有音效路径（按你实际文件填写）
+const preloadSounds = [
+    "./sfx/0/c.ogg",
+    "./sfx/0/e.ogg",
+    "./sfx/0/d.ogg",
+    "./sfx/0/Hx.ogg",
+    "./sfx/0/f.ogg",
+    "./sfx/0/b.ogg",
+    "./sfx/0/g.ogg",
+    "./sfx/0/a.ogg",
+    "./sfx/0/tetris",
+    "sfx/kus.ogg"
+];
+
+// 页面加载完成后，批量预加载音效
+window.addEventListener("load", function() {
+    preloadSounds.forEach(function(src) {
+        const tempAudio = new Audio(src);
+        tempAudio.preload = "auto"; // 强制提前加载
+        tempAudio.load();
+    });
+});
